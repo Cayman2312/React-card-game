b474fb1b132a9d0ca888eedd261b644298990b00
M.Studitskiy <cayman2312@gmail.com>
2020-05-03 16:57:34 +0300

    прикрутил выбор (range) при выборе количества карт

--- src/containers/Popup.js
+++ src/containers/Popup.js
@@ -1,35 +1,22 @@
 import React, { Component } from 'react';
 import './Popup.scss';
+import { Range, getTrackBackground } from 'react-range';
+
+const STEP = 2;
+const MIN = 2;
+const MAX = 40;
 
 export default class Popup extends Component {
   constructor(props) {
     super(props);
-    this.ref = React.createRef();
     this.state = {
-      isInputValid: false,
-      cardNumber: null,
+      values: [20],
     };
-  }
-
-  componentDidMount() {
-    this.ref.current.focus();
-  }
-
-  validate(value) {
-    let isInputValid = true;
-    value = value.trim();
-    if (isNaN(value - 1) || value <= 0 || value % 2 !== 0 || value > 40) {
-      isInputValid = false;
-    }
-
-    this.setState({ isInputValid: isInputValid, cardNumber: value });
   }
 
   onSubmit(e, number) {
     e.preventDefault();
-    if (!this.state.isInputValid) {
-      return;
-    }
+
     this.props.setCardAmount(number);
   }
 
@@ -38,30 +25,84 @@ export default class Popup extends Component {
       <div className="Popup">
         <div className="Popup__window">
           <h3 className="Popup__title">
-            Введите желаемое количество карточек:
+            Выберите желаемое количество карточек:
           </h3>
           <form
             action=""
             onSubmit={(e) => {
-              this.onSubmit(e, this.state.cardNumber);
+              this.onSubmit(e, this.state.values[0]);
             }}
           >
-            <input
-              ref={this.ref}
-              type="text"
-              className="Popup__input"
-              id="input"
-              onChange={(e) => {
-                this.validate(e.target.value);
-              }}
+            <Range
+              values={this.state.values}
+              step={STEP}
+              min={MIN}
+              max={MAX}
+              onChange={(values) => this.setState({ values })}
+              renderTrack={({ props, children }) => (
+                <div
+                  onMouseDown={props.onMouseDown}
+                  onTouchStart={props.onTouchStart}
+                  style={{
+                    ...props.style,
+                    height: '36px',
+                    display: 'flex',
+                    width: '100%',
+                  }}
+                >
+                  <div
+                    ref={props.ref}
+                    style={{
+                      height: '5px',
+                      width: '100%',
+                      borderRadius: '4px',
+                      background: getTrackBackground({
+                        values: this.state.values,
+                        colors: ['#548BF4', '#ccc'],
+                        min: MIN,
+                        max: MAX,
+                      }),
+                      alignSelf: 'center',
+                    }}
+                  >
+                    {children}
+                  </div>
+                </div>
+              )}
+              renderThumb={({ props, isDragged }) => (
+                <div
+                  {...props}
+                  style={{
+                    ...props.style,
+                    height: '42px',
+                    width: '42px',
+                    borderRadius: '4px',
+                    backgroundColor: '#FFF',
+                    display: 'flex',
+                    justifyContent: 'center',
+                    alignItems: 'center',
+                    boxShadow: '0px 2px 6px #AAA',
+                  }}
+                >
+                  <div
+                    style={{
+                      height: '16px',
+                      width: '5px',
+                      backgroundColor: isDragged ? '#548BF4' : '#CCC',
+                    }}
+                  />
+                </div>
+              )}
             />
+            <output
+              style={{ marginTop: '10px', fontWeight: 'bold', fontSize: '2em' }}
+              id="output"
+            >
+              {this.state.values[0]}
+            </output>
+            <br />
             <input type="submit" className="Popup__submit" value="Старт!" />
           </form>
-          <p
-            className={`Popup__validation ${this.state.isInputValid.toString()}`}
-          >
-            *введите четное целое число не больше 40
-          </p>
         </div>
       </div>
     );
